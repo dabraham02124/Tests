f8f006b5cdbc1a489ad1045c2458ab2dd0f0d2e6
Daniel Abraham <dabraham@sweatshop.org>
2014-05-24 20:37:39 -0400

    added Java 8 stuff

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Tests
 -----
 
 Just a bunch of tests to make sure I have the exact ins and outs of lots of silly little things correct.
+
+Requirements
+------------
+* Maven to build
+* Java 8 for (at the moment) one file
---
+++ src/main/java/org/sweatshop/tests/Java8Tester.java
@@ -0,0 +1,66 @@
+package org.sweatshop.tests;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Java8Tester {
+
+    private static List<String> createStringList() {
+        List<String> stringList = new LinkedList<>();
+        stringList.add("foo");
+        stringList.add("bar");
+        stringList.add("baz");
+        stringList.add("qux");
+        stringList.add("quux");
+        stringList.add("quuux");
+        return Collections.unmodifiableList(stringList);
+    }
+    
+    private static List<Integer> createIntegerList() {
+        List<Integer> intList = new LinkedList<>();
+        intList.add(5);
+        intList.add(6);
+        intList.add(7);
+        intList.add(8);
+        intList.add(9);
+        return Collections.unmodifiableList(intList);
+    }
+
+
+    private static void forEachTester() {
+        createIntegerList().forEach((a) -> System.out.println(a*a));
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.replaceAll(i -> i*i);
+        list.add(3);
+        System.out.println(list);
+        
+        
+        forEachTester();System.out.println();
+        filterTester();System.out.println();
+        mapTester();System.out.println();
+        alterListInPlace();System.out.println();
+    }
+    
+    private static void alterListInPlace() {
+        List<String> stringList = new LinkedList<>();
+        createStringList().forEach(s -> stringList.add(s));
+        stringList.replaceAll(s -> s.toUpperCase());
+        System.out.println(stringList);
+    }
+
+    private static void mapTester() {
+        System.out.println(createIntegerList().stream().map(i -> i*i).collect(Collectors.toList()));
+
+    }
+    
+    private static void filterTester() {
+        createStringList().stream().filter(s -> s.length() > 3).forEach(s -> System.out.println(s));
+    }
+}
