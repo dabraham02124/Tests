f2711d20f9e51e7068343ee03f7b6f09ca76c006
Daniel Malcolm Abraham <abrahamd@akamai.com>
2015-06-24 10:22:25 -0400

    removed warnings

--- src/main/java/org/sweatshop/tests/GsonTest.java
+++ src/main/java/org/sweatshop/tests/GsonTest.java
@@ -1,19 +1,17 @@
 package org.sweatshop.tests;
 
+import lombok.Value;
+
 import com.google.gson.Gson;
 
+@Value
 public class GsonTest {
-    
     private String foo;
     private Integer bar;
     private int baz;
     
-    
     public static void main(String[] args) {
-        GsonTest gt = new GsonTest();
-        gt.foo = "fooString";
-        gt.bar = 17;
-        gt.baz = 94;
+        GsonTest gt = new GsonTest("fooString", 17, 94);
         
         String json = new Gson().toJson(gt);
         
@@ -22,23 +20,5 @@ public class GsonTest {
         GsonTest gt2 = new Gson().fromJson(json,GsonTest.class);
         
         if (gt.equals(gt2)) System.out.println("equals!");
-        
     }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof GsonTest) {
-            GsonTest g2 = (GsonTest) obj;
-            if (!foo.equals(g2.foo)) return false;
-            if (!bar.equals(g2.bar)) return false;
-            if (baz != (g2.baz)) return false;
-            
-        return true; 
-        }
-        return false;
-        
-    }
-    
-
 }
